55561c7914e6f87f43c37158b9d2a5ce0196f941
Andre Hochi <andrehochiwork@gmail.com>
2021-02-12 01:59:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,11 +5,3 @@ end
 def read_from_hash(hash, key)
   hash[key]
 end
-
-def update_counting_hash(hash, key)
-  new_hash = hash
-  if !hash[key]
-    new_hash[key] = 1
-  else 
-    new_hash[key] = hash[key] + 1
-end
